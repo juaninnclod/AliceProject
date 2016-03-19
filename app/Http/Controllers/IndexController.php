6368be9933072d6def37edf7fd9019d8eb5366fc
Juan Gutierrez <juangutierrez100@gmail.com>
2016-03-19 13:28:07 -0500

    Create index controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+  public function index()
+  {
+    
+  }
+}
